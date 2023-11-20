2c3cafba674dcd912c04c19ce40549a6080439ab
Neider123 <neiderjoansesc@ufps.edu.co>
2023-11-20 12:26:53 -0500

    segundo commit

--- src/main/java/co/empresa/bbva/dao/BillDao.java
+++ src/main/java/co/empresa/bbva/dao/BillDao.java
@@ -1,11 +1,13 @@
 package co.empresa.bbva.dao;
 
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import co.empresa.bbva.modelo.Bill;
 import co.empresa.bbva.modelo.User;
 import co.empresa.bbva.util.Conexion;
 
@@ -13,22 +15,24 @@ public class BillDao {
 	
 	private Conexion conexion;
 	
-	private static final String INSERT_BILL_SQL = "INSERT INTO user(username,pass,email) VALUES (?,?,?);";
-	private static final String UPDATE_BILL_SQL = "DELETE FROM user WHERE id = ?;";
-	private static final String DELETE_BILL_SQL = "UPDATE user SET username = ? , pass= ?,email=? ; ";
-	private static final String SELECT_BILL_BY_ID = "SELECT * FROM user WHERE id= ? ;";
-	private static final String SELECT_ALL_BILL = "SELECT * FROM user;";
+	private static final String INSERT_BILL_SQL = "INSERT INTO bill(date_bill,user_id,value,type,observation) VALUES (?,?,?,?,?);";
+	private static final String UPDATE_BILL_SQL = "DELETE FROM bill WHERE id = ?;";
+	private static final String DELETE_BILL_SQL = "UPDATE bill SET date_bill = ? , user_id = ?,value= ? , type=?,observation=?; ";
+	private static final String SELECT_BILL_BY_ID = "SELECT * FROM bill WHERE id= ? ;";
+	private static final String SELECT_ALL_BILL = "SELECT * FROM bill;";
 	
 	public BillDao() {
 		this.conexion = conexion.getConexion();
 	}
 	
-	public void insert(User usuario) {
+	public void insert(Bill bill) {
 		try {
-			PreparedStatement preparedStatement = (PreparedStatement) conexion.setPreparedStatement(INSERT_USUARIO_SQL);
-			preparedStatement.setString(1, usuario.getUsername());
-			preparedStatement.setString(2, usuario.getPass());
-			preparedStatement.setString(3, usuario.getEmail());
+			PreparedStatement preparedStatement = (PreparedStatement) conexion.setPreparedStatement(INSERT_BILL_SQL);
+			preparedStatement.setDate(1,bill.getDate_bill());
+			preparedStatement.setInt(2,bill.getUser_id());
+			preparedStatement.setInt(3,bill.getValue());
+			preparedStatement.setInt(4,bill.getType());
+			preparedStatement.setString(5,bill.getObservation());
 			conexion.execute();
 		}catch(SQLException e) {
 			
@@ -37,7 +41,7 @@ public class BillDao {
 	
 	public void delete(int id) {
 		try {
-			PreparedStatement preparedStatement = (PreparedStatement) conexion.setPreparedStatement(DELETE_USUARIO_SQL);
+			PreparedStatement preparedStatement = (PreparedStatement) conexion.setPreparedStatement(DELETE_BILL_SQL);
 			preparedStatement.setInt(1,id);
 			conexion.execute();
 		}catch(SQLException e) {
@@ -45,36 +49,38 @@ public class BillDao {
 		}
 	}
 	
-	public void update(User usuario) {
+	public void update(Bill bill) {
 		try {
-			PreparedStatement preparedStatement = (PreparedStatement) conexion.setPreparedStatement(INSERT_USUARIO_SQL);
-			preparedStatement.setString(1, usuario.getUsername());
-			preparedStatement.setString(2, usuario.getPass());
-			preparedStatement.setString(3, usuario.getEmail());
-			preparedStatement.setInt(4, usuario.getId());
+			PreparedStatement preparedStatement = (PreparedStatement) conexion.setPreparedStatement(INSERT_BILL_SQL);
+			preparedStatement.setDate(1,bill.getDate_bill());
+			preparedStatement.setInt(2,bill.getUser_id());
+			preparedStatement.setInt(3,bill.getValue());
+			preparedStatement.setInt(4,bill.getType());
+			preparedStatement.setString(5,bill.getObservation());;
+			preparedStatement.setInt(6, bill.getId());
 		}catch(SQLException e) {
 			
 		}
 	}
 	
-	public List<User> selectAll(){
-		List <User> usuarios = new ArrayList<>();
+	public List<Bill> selectAll(){
+		List <Bill> bills = new ArrayList<>();
 		try {
-			PreparedStatement preparedStatement = (PreparedStatement) conexion.setPreparedStatement(SELECT_ALL_USUARIOS);
+			PreparedStatement preparedStatement = (PreparedStatement) conexion.setPreparedStatement(SELECT_ALL_BILL);
 			
 			ResultSet rs = conexion.query();
 			while(rs.next()) {
 				int id = rs.getInt("id");
-				String username = rs.getString("username");
-				String pass = rs.getString("pass");
-				String email = rs.getString("email");
-				usuarios.add(new User(id,username,pass,email));
+				Date date_bill = rs.getDate("date_bill");
+				Integer user_id = rs.getInt("user_id");
+				Integer value  = rs.getInt("value");
+				bills.add(new Bill(id,date_bill,user_id,value));
 			}
 
 		}catch(SQLException e) {
 			
 		}
-		return usuarios;
+		return bills;
 		
 	}
 	
@@ -82,7 +88,7 @@ public class BillDao {
 		User usuario = null;
 		List <User> usuarios = new ArrayList<>();
 		try {
-			PreparedStatement preparedStatement = (PreparedStatement) conexion.setPreparedStatement(SELECT_USUARIO_BY_ID);
+			PreparedStatement preparedStatement = (PreparedStatement) conexion.setPreparedStatement(SELECT_BILL_BY_ID);
 			preparedStatement.setInt(1, id);
 			ResultSet rs = conexion.query();
 			while(rs.next()) {
